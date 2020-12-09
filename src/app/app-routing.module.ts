33b34506bc485b1416219b9bf112e05c1300f85d
meilie <meilie@meilie-ThinkPad-L560>
2020-12-10 10:19:16 +1100

    ajout route de la route 'search' (écran d'accueil de recherche d'un colis) redirection vers la route 'search' rechargement de la page si l'on recoit une requete sur l'URL courante #1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/search', pathMatch: 'full'},
+  { path: 'search', loadChildren: () => import('./view/search/search.module').then(m => m.SearchModule)},
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { onSameUrlNavigation: 'reload' })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
