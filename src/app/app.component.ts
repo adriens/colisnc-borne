eb94ebaaeb0d0b81bc087147e7e47b2cd69e96b5
meilie <meilie@meilie-ThinkPad-L560>
2020-12-10 10:24:36 +1100

    modification App Component création de nouveau component accessibles via des routes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,4 @@
-import {Component, AfterViewInit} from '@angular/core'
-import {FormControl, FormGroup, Validators} from '@angular/forms'
-import { Router } from '@angular/router';
-
-import {ProgressSpinnerMode} from '@angular/material/progress-spinner';
-
-import { ColisService } from './service/colis.service';
-import { ThemePalette } from '@angular/material/core';
+import {Component} from '@angular/core'
 
 @Component({
   selector: 'app-root',
@@ -14,35 +7,6 @@ import { ThemePalette } from '@angular/material/core';
 })
 export class AppComponent {
 
-  color: ThemePalette = 'primary';
-  mode: ProgressSpinnerMode = 'determinate';
-  value = 50;
-
-  title = 'colisnc-borne';
-
-  textInput = '';
-
-  loading: boolean = false;
-
-  colis = [];
-  showColis: boolean = false;
-
-  exampleForm = new FormGroup({
-    requiredInput: new FormControl('', [Validators.email]),
-    disabledInput: new FormControl({value: 'Value from formcontrol', disabled: true}),
-    textInput: new FormControl(''),
-  });
-
-  constructor(private router: Router, private colisService: ColisService) { }
-
-  search(value) {
-    this.loading = true;
-    this.textInput = value;
-    this.colisService.getColis(this.textInput).subscribe(colis => {
-      this.colis = colis;
-      this.showColis = true;
-      this.loading = false;
-    });
-  }
+  constructor() { }
 
 }
