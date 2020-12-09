a98e31e218067252df7ff756507017ad0d5c34ce
meilie <meilie@meilie-ThinkPad-L560>
2020-12-10 10:31:09 +1100

    Update search.module.ts

---
+++ src/app/view/search/search.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import {SearchRoutingModule} from "./search.route.module";
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    SearchRoutingModule
+  ],
+  exports: []
+})
+export class SearchModule { }
