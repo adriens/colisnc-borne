252d1fbdf404754b4027784be112f261ed76af95
meilie389 <bossart.emilie@gmail.com>
2020-12-21 09:10:53 +1100

    modification type Colis #1

--- src/app/view/colis/colis.component.ts
+++ src/app/view/colis/colis.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { Colis } from '../../model/colis.model';
 
 @Component({
   selector: 'app-colis',
@@ -7,7 +8,7 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class ColisComponent implements OnInit {
 
-  @Input() coli: any;
+  @Input() coli: Colis;
 
   constructor() { }
 
